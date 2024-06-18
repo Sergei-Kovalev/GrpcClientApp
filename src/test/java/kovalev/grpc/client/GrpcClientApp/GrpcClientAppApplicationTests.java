facb5c3aee66c2cf928f957de212cab47569eb52
Sergey Kovalev <s.kovalev.jdev@gmail.com>
2024-06-18 15:46:56 +0300

    Initial commit

---
+++ src/test/java/kovalev/grpc/client/GrpcClientApp/GrpcClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kovalev.grpc.client.GrpcClientApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcClientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
