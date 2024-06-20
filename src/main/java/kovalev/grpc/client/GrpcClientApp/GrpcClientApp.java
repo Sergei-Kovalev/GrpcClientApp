4698e157f7c74c972e7a5ca7624c93b2330850a4
Sergey Kovalev <s.kovalev.jdev@gmail.com>
2024-06-20 13:01:09 +0300

    client ended... I think so.

--- src/main/java/kovalev/grpc/client/GrpcClientApp/GrpcClientApp.java
+++ src/main/java/kovalev/grpc/client/GrpcClientApp/GrpcClientApp.java
@@ -1,53 +1,17 @@
 package kovalev.grpc.client.GrpcClientApp;
 
-import com.sample.GrpcService;
-import com.sample.UserServiceGrpc;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
+import kovalev.grpc.client.GrpcClientApp.controller.ClientAppController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.concurrent.TimeUnit;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GrpcClientApp {
 
 	public static void main(String[] args) throws InterruptedException {
-		SpringApplication.run(GrpcClientApp.class, args);
-		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
-				.usePlaintext().build();
+		ConfigurableApplicationContext context = SpringApplication.run(GrpcClientApp.class, args);
 
-		UserServiceGrpc.UserServiceBlockingStub stub = UserServiceGrpc.newBlockingStub(channel);
-
-		for (int i = 0; i < 10; i++) {
-			long id = (long) (Math.random() * 15);
-
-			System.out.println("Клиент запрашивает рандомное ID = " + id);
-
-			GrpcService.FindByIDRequest request = GrpcService.FindByIDRequest.newBuilder()
-					.setId(id)
-					.build();
-
-			GrpcService.FindByIDResponse response = stub.findById(request);
-
-			switch (response.getResponseCase()) {
-				case SUCCESS_RESPONSE -> System.out.printf("""
-					Response was success. Your Entity:
-					%s
-					""", response.getSuccessResponse());
-				case ERROR_RESPONSE -> System.out.printf("""
-					Error from server. INFO:
-					%s
-					""", response.getErrorResponse().getErrorDescription());
-			}
-			System.out.println("""
-					
-					----------------------------------------------
-					
-					""");
-
-			TimeUnit.SECONDS.sleep(1);
-		}
-		channel.shutdownNow();
+		ClientAppController controller = context.getBean(ClientAppController.class);
+		controller.sendRequests();
 	}
 }
---
+++ src/main/java/kovalev/grpc/client/GrpcClientApp/controller/ClientAppController.java
@@ -0,0 +1,59 @@
+package kovalev.grpc.client.GrpcClientApp.controller;
+
+import com.sample.GrpcService;
+import com.sample.UserServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.springframework.stereotype.Controller;
+
+import java.util.concurrent.TimeUnit;
+
+@Controller
+public class ClientAppController {
+
+    public static final String RANDOM_ID_MESSAGE = "Клиент запрашивает рандомное ID = ";
+    public static final String SUCCESS_RESPONSE_TEMPLATE = """
+            Response was success. Your Entity:
+            %s
+            """;
+    public static final String ERROR_RESPONSE_TEMPLATE = """
+            Error from server. INFO:
+            %s
+            """;
+    public static final String END_MESSAGE_LINE = """
+            					
+            ----------------------------------------------
+            					
+            """;
+    public static final int REQUESTS_COUNT = 10;
+    public static final int MAX_REQUESTED_ID = 15;
+    public static final int TIME_BETWEEN_REQUESTS_SEC = 1;
+
+    public void sendRequests() throws InterruptedException {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
+                .usePlaintext().build();
+
+        UserServiceGrpc.UserServiceBlockingStub stub = UserServiceGrpc.newBlockingStub(channel);
+
+        for (int i = 0; i < REQUESTS_COUNT; i++) {
+            long id = (long) (Math.random() * MAX_REQUESTED_ID + 1);
+
+            System.out.println(RANDOM_ID_MESSAGE + id);
+
+            GrpcService.FindByIDRequest request = GrpcService.FindByIDRequest.newBuilder()
+                    .setId(id)
+                    .build();
+
+            GrpcService.FindByIDResponse response = stub.findById(request);
+
+            switch (response.getResponseCase()) {
+                case SUCCESS_RESPONSE -> System.out.printf(SUCCESS_RESPONSE_TEMPLATE, response.getSuccessResponse());
+                case ERROR_RESPONSE -> System.out.printf(ERROR_RESPONSE_TEMPLATE, response.getErrorResponse().getErrorDescription());
+            }
+            System.out.print(END_MESSAGE_LINE);
+
+            TimeUnit.SECONDS.sleep(TIME_BETWEEN_REQUESTS_SEC);
+        }
+        channel.shutdownNow();
+    }
+}
